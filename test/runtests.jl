a4ec8d42aab50aa7135d13fbdb5d8441bfa8f7ec
Sven Duve <svenduve@gmail.com>
2023-08-16 12:14:44 +0200

    adjusted test

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,6 +2,16 @@ using DDPG
 using RLTypes
 using Flux
 using Test
+using Conda
+using PyCall
+
+begin 
+    Conda.pip_interop(true)
+    Conda.add("wheel")
+    Conda.add("box2d-py")
+    Conda.add("pygame")
+    Conda.pip("install", "git+https://github.com/SvenDuve/Gymnasium")
+end
 
 @testset "DDPG.jl" begin
     # Write your tests here.
